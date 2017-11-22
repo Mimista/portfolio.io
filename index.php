cbfd2e79e1a1e9d27d2693b031ae7f3d278f3a0e
hakkeriino <30336435+hakkeriino@users.noreply.github.com>
2017-11-22 15:41:48 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,68 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Testi</title>
+	<link rel="stylesheet" href="style.css">
+	<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+	<div id="wrapper">
+<div id="header">
+	<div id="icons">
+		<a href="https://github2.com/hakkeriino/">
+		<img src="kuvat/github.png" class="icon2"></a>
+
+		<a href="https://twitter.com">
+		<img src="kuvat/twitter.png" class="icon1"></a>
+
+
+		<a href="https://www.linkedin.com/in/mirasalenius/">
+		<img src="kuvat/linkedin.png" class="icon3"></a>
+	</div>
+
+
+	<div id="head">
+			<div id="title">Mira Salenius</div>
+			<div id="bodyText">LINUX | DEV | ADMIN </div>
+	</div></div>
+	
+<hr style="width:50%">
+
+	<div id="menu">
+		<ul>
+		  <li><a href="home.php">About</a></li>
+		  <li><a href="skills.php">Skills</a></li>
+		  <li><a href="contact.php">Contact</a></li>
+		  <li><a href="blog.php">Blog</a></li>
+		</ul>
+
+	</div>
+
+
+
+
+	<div id="container">
+		<img src="kuvat/profiili.jpg" style=" width: 150px; height:150px;border-radius: 50%;display: inline;
+    position: absolute; margin-top: 25px;margin-left: 40px;">
+		<div id="text1"><p class="ex">Hello! My name is Mira Salenius, and I'm a third year student graduating from Kajaani University of Applied Sciences the next spring. At the moment I'm searching for a internship/trainee place. I'm interested in web development, Linux and sysadmin sides. I have a varied set of skills ranging from HTML + CSS + PHP, Linux, and servers </p></div>
+</div>
+
+
+</div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
